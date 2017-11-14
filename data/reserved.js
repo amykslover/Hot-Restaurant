a305a3f50674465de8f4139b6234a2f19405da6b
mriddle86 <marshall.riddle@gmail.com>
2017-11-14 14:00:10 -0800

    front end

---
+++ data/reserved.js
@@ -0,0 +1,9 @@
+var reservedTable = [
+  {
+   	name: "Customer Person",
+    phone: "222-2222",
+    email: "test@test.com"
+    
+  }];
+
+  module.exports = reservedTable;
---
+++ data/waiting.js
@@ -0,0 +1,9 @@
+var waitingTable = [
+  {
+   	name: "Customer Gal",
+    phone: "111-2222",
+    email: "test2@test.com"
+    
+  }];
+
+  module.exports = waitingTable;
