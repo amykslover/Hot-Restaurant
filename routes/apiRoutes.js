3bea049b5a0041ab9a78ce46d97e5aeff6156c54
Amy Slover <amykslover@gmail.com>
2017-11-14 13:55:25 -0800

    Added routing and pushing data to reservations array

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,20 @@
+// var app = require('./server')
+module.exports = function(app){
 
+	var reservations = [];
 
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+	// Create New Reservatoins - takes in JSON input
+	app.post("/api/new", function(req, res) {
+	  // req.body hosts is equal to the JSON post sent from the user
+	  // This works because of our body-parser middleware
+	  var newReservation = req.body;
+	  newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
 
-// Create New Reservatoins - takes in JSON input
-app.post("/api/new", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body-parser middleware
-  var newRes = req.body;
-  newRes.routeName = newRes.name.replace(/\s+/g, "").toLowerCase();
-
-  console.log(newRes);
-  //if  5 tables push to other array
-  reservations.push(newRes);
-  res.json(newRes);
-});
+	  console.log(newReservation);
+	  //if  5 tables push to other array
+	  reservations.push(newReservation);
+	  console.log(reservations)
+	  res.json(newReservation);
+	});
+	
+}
